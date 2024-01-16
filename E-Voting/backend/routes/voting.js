459c6d81331fb143b06db3b2b9686f6f98d2f84a
chirag-suvarna <chiragsuvarna74@gmail.com>
2024-01-16 04:09:58 +0000

    definned controller

--- E-Voting/backend/routes/voting.js
+++ E-Voting/backend/routes/voting.js
@@ -1,38 +1,28 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/userModel')
+const
+    {
+        getUsers,
+        getUser,
+        createUser,
+        deleteUser,
+        updateUser,
+    }= require('../controllers/userController')
 
 // Get all voters
-router.get('/',(req,res)=>{
-    res.json({msg:'Get all voter'});
-})
+router.get('/',getUsers)
 
 //Get single voter
-router.get('/:id',(req,res)=>{
-    res.json({msg:'Get single voter'});
-})
+router.get('/:id',getUser)
 
 //post a voter
-router.post('/',async(req,res)=>{
-    const{first_name,last_name,contact_no,email_id,password,date_of_birth,is_verfied,is_admin}=req.body
-    try{
-        const user = await User.create({first_name,last_name,contact_no,email_id,password,date_of_birth,is_verfied,is_admin})
-        res.status(200).json(user)
-    }
-    catch(error){
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/',createUser)
 
 //delete a voter
-router.delete('/:id',(req,res)=>{
-    res.json({msg:'delete a voter'});
-})
+router.delete('/:id',deleteUser)
 
 //update a voter
-router.patch('/:id',(req,res)=>{
-    res.json({msg:'update a voter'});
-})
+router.patch('/:id',updateUser)
 
 
 module.exports = router
