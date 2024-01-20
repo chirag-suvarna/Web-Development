b6556b0267d77060e898b5221bab501d633142b1
chirag-suvarna <chiragsuvarna74@gmail.com>
2024-01-20 00:45:11 +0000

    init blockchain

--- E-Voting/clients/src/App.js
+++ E-Voting/clients/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
-import RegistrationForm from './components/RegistrationForm';
-import { Toaster } from 'react-hot-toast';
+import React from "react";
+import "./App.css";
+import Main from "./pages/Main";
+// import RegistrationForm from './components/RegistrationForm';
+// import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-    <Toaster position="top-center" toastOptions={{duration:2000}}/>
-    <RegistrationForm/>
+      {/* <Toaster position="top-center" toastOptions={{duration:2000}}/> */}
+      {/* <RegistrationForm/> */}
+      <Main />
     </>
   );
 }
---
+++ E-Voting/contracts/voting.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+contract voting{
+    uint public votingCount = 0;
+
+
+}
