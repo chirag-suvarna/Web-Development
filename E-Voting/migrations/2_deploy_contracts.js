46b0dd7f3597b330859cf3a7336704e346e38d64
Chirag Suvarna <chiragsuvarna74@gmail.com>
2024-01-21 21:59:41 +0000

    Delete E-Voting directory

--- E-Voting/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const Voting = artifacts.require(
-  path.resolve(__dirname, "contracts", "Voting.sol")
-); // Adjust the path based on your project structure
-
-module.exports = function (deployer) {
-  deployer.deploy(Voting);
-};
